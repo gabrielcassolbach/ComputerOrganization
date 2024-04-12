4b39421aebaf03dc4edfc94c72e88552747fa937
gabrielcassolbach <gabrielcassolbach@gmail.com>
2024-04-12 15:46:49 -0300

    ula at initial tests ready

--- ULA/test/ula_tb.vhd
+++ ULA/test/ula_tb.vhd
@@ -1,5 +1,6 @@
 Library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all; 
 
 entity ula_tb is
 end entity;
@@ -8,25 +9,38 @@ architecture testbench of ula_tb is
 
 --component 
 component ula is 
-port (  data1_in: in std_logic_vector (15 downto 0);
-        data2_in: in std_logic_vector (15 downto 0);
-        operation: in std_logic_vector (3 downto 0);
-        data3_out: out std_logic_vector (15 downto 0));
+port (  data1_in: in unsigned (15 downto 0);
+        data2_in: in unsigned (15 downto 0);
+        sel_op: in unsigned(1 downto 0);
+        data3_out: out unsigned (15 downto 0));
 end component;
 
 --signals
-signal data1_in: std_logic_vector (15 downto 0);
-signal data2_in: std_logic_vector (15 downto 0);
-signal operation: std_logic_vector (3 downto 0);
-signal data3_out: std_logic_vector (15 downto 0);
+signal data1_in: unsigned (15 downto 0);
+signal data2_in: unsigned (15 downto 0);
+signal sel_op: unsigned   (1 downto 0);
+signal data3_out: unsigned (15 downto 0);
 
     begin 
-    ula01: ula port map (data1_in, data2_in, operation, data3_out);
+    ula01: ula port map (data1_in, data2_in, sel_op, data3_out);
 
     process 
     begin 
         data1_in <= "0000000000000000";
         data2_in <= "0000000000000001";
+        sel_op <= "00";
+        wait for 5 ns;
+        data1_in <= "0000000000000000";
+        data2_in <= "0000000000000001";
+        sel_op <= "01";
+        wait for 5 ns;
+        data1_in <= "0000000000000000";
+        data2_in <= "0000000000000001";
+        sel_op <= "10";
+        wait for 5 ns;
+        data1_in <= "0000000000000000";
+        data2_in <= "0000000000000001";
+        sel_op <= "11";
         wait for 5 ns;
     end process;
 
