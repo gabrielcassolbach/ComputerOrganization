b0048fd1a59b258060976b2adfa67efb1ad7c81b
Adame <gusadomingues@gmail.com>
2024-04-14 17:05:14 -0300

    feat - carry & overflow added

--- ULA/test/ula_tb.vhd
+++ ULA/test/ula_tb.vhd
@@ -7,22 +7,31 @@ end entity;
 
 architecture testbench of ula_tb is 
 
---component 
-component ula is 
-port (  data1_in: in unsigned (15 downto 0);
-        data2_in: in unsigned (15 downto 0);
-        sel_op: in unsigned(1 downto 0);
-        data3_out: out unsigned (15 downto 0));
-end component;
+    --component 
+    component ula is 
+        port (  
+            data1_in, data2_in : in unsigned (15 downto 0);
+            sel_op             : in unsigned(1 downto 0);
+            data3_out          : out unsigned (15 downto 0);
+            carry              : out std_logic;
+            overflow           : out std_logic
+    );
+    end component;
 
---signals
-signal data1_in: unsigned (15 downto 0);
-signal data2_in: unsigned (15 downto 0);
-signal sel_op: unsigned   (1 downto 0);
-signal data3_out: unsigned (15 downto 0);
+    --signals
+    signal data1_in, data2_in, data3_out : unsigned (15 downto 0);
+    signal carry, overflow : std_logic;
+    signal sel_op : unsigned (1 downto 0);
 
-    begin 
-    ula01: ula port map (data1_in, data2_in, sel_op, data3_out);
+begin 
+    ula01: ula port map (
+        data1_in => data1_in,
+        data2_in => data2_in,
+        sel_op => sel_op,
+        data3_out => data3_out,
+        carry => carry,
+        overflow => overflow
+    );
 
     process 
     begin 
@@ -42,6 +51,21 @@ signal data3_out: unsigned (15 downto 0);
         data2_in <= "0000000000000001";
         sel_op <= "11";
         wait for 5 ns;
+        data1_in <= "0000000000000111";
+        data2_in <= "0000000000000111";
+        sel_op <= "00";
+        wait for 5 ns;
+        data1_in <= "0000000000000111";
+        data2_in <= "0000000000000111";
+        sel_op <= "01";
+        wait for 5 ns;
+        data1_in <= "0000000000000111";
+        data2_in <= "0000000000000111";
+        sel_op <= "10";
+        wait for 5 ns;
+        data1_in <= "0000000000000111";
+        data2_in <= "0000000000000111";
+        sel_op <= "11";
+        wait for 5 ns;
     end process;
-
     end testbench;
--- ULA/ula.vhd
+++ ULA/ula.vhd
@@ -4,19 +4,45 @@ use ieee.numeric_std.all;
 
 entity ula is 
     port (
-        data1_in: in unsigned (15 downto 0);
-        data2_in: in unsigned (15 downto 0);
-        sel_op: in unsigned(1 downto 0);
-        data3_out: out unsigned (15 downto 0)
+        data1_in, data2_in : in unsigned (15 downto 0);      --entrada de dados
+        sel_op             : in unsigned(1 downto 0);        --seleção de operação
+        data3_out          : out unsigned (15 downto 0);     --resultado da operação
+        carry              : out std_logic;                  --carry
+        overflow           : out std_logic                   --overflow
     );
 end ula;
 
-architecture struct of ula is 
+architecture struct of ula is
 
-    begin 
-    data3_out <= (data1_in + data2_in)   when sel_op="00" else 
-                  (data1_in - data2_in)   when sel_op="01" else 
-                  (data1_in xor data2_in) when sel_op="10" else 
-                  (data1_in and data2_in) when sel_op="11" else 
-                "0000000000000000";
-    end struct;
+    -- variáveis temporárias
+    signal data1_temp, data2_temp, sum_temp, sub_temp : unsigned(16 downto 0);
+
+begin
+
+    data1_temp <= '0' & data1_in;
+    data2_temp <= '0' & data2_in;
+    sum_temp <= data1_temp + data2_temp;
+    sub_temp <= data1_temp - data2_temp;
+
+    -- resultado da operação
+    data3_out <=    sum_temp(15 downto 0)   when sel_op="00" else 
+                    sub_temp(15 downto 0)   when sel_op="01" else 
+                    (data1_in xor data2_in) when sel_op="10" else 
+                    (data1_in and data2_in) when sel_op="11" else 
+                    "0000000000000000";
+    
+    -- carry (quando a operação é de subtração, o carry é 1 se data2_in > data1_in, 0 caso contrário)
+    carry <=    sum_temp(16) when sel_op = "00"                          else
+                '0'          when sel_op = "01" and data2_in <= data1_in else
+                '1'          when sel_op = "01" and data2_in > data1_in  else
+                '0';
+
+    -- overflow
+    overflow <= '1' when sel_op = "00" and data1_in(15) = '0' and data2_in(15) = '0' and sum_temp(15) = '1' else
+                '1' when sel_op = "00" and data1_in(15) = '1' and data2_in(15) = '1' and sum_temp(15) = '0' else
+                '1' when sel_op = "01" and data1_in(15) = '0' and data2_in(15) = '1' and sub_temp(15) =  '1' else
+                '1' when sel_op = "01" and data1_in(15) = '1' and data2_in(15) = '0' and sub_temp(15) =  '0' else
+                '0';
+
+
+end struct;
