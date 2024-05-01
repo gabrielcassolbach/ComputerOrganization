be02ee97305ea171649fb9e598607a8e179701e8
Adame <gusadomingues@gmail.com>
2024-05-01 18:54:58 -0300

    fix - adder without clk

--- program_counter/adder.vhd
+++ program_counter/adder.vhd
@@ -3,18 +3,13 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity adder is
-    port( clk      : in std_logic;
-          data_in  : in unsigned  (15 downto 0);
-          data_out : out unsigned (15 downto 0)
+    port(
+          data_in  : in unsigned  (6 downto 0);
+          data_out : out unsigned (6 downto 0)
     );
  end entity;
  
  architecture struct of adder is 
-     begin
-        process(clk)
-        begin
-            if(rising_edge(clk)) then
-                data_out <= data_in + 1; 
-            end if;
-        end process;
-     end struct;
+    begin
+        data_out <= data_in + 1;
+end architecture;
