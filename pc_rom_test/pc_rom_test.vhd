2833567c88520198da456aa14d23550489bb10e5
Adame <gusadomingues@gmail.com>
2024-05-03 16:12:26 -0300

    fix - variables naming

--- pc_rom_test/pc_rom_test.vhd
+++ pc_rom_test/pc_rom_test.vhd
@@ -5,13 +5,15 @@ use ieee.numeric_std.all;
 entity test is
     port (
         clk : in std_logic;
-        wr_en : in std_logic;
+        pc_wr : in std_logic;
         rst : in std_logic;
+        cu_state : out std_logic;    -- for debugging
         test_data_rom_out : out unsigned(15 downto 0)
     );
 end entity test;
 
 architecture test_a of test is
+    -- component declarations
     component program_counter is
         port (
             clk : in std_logic;
@@ -20,6 +22,7 @@ architecture test_a of test is
             data_out : out unsigned(6 downto 0)
         );
     end component program_counter;
+
     component rom is
         port (
             clk : in std_logic;
@@ -27,16 +30,33 @@ architecture test_a of test is
             data : out unsigned(15 downto 0)
         );
     end component rom;
+
+    component control_unit is
+        port(
+            clk     : in std_logic;
+            rst     : in std_logic;
+            state   : out std_logic;    -- for debugging
+            pc_wr   : out std_logic
+        );
+    end component control_unit;
     
     -- signal declarations
     signal data_out_s: unsigned(6 downto 0);
+    signal pc_wr_s: std_logic;
 
 begin
     -- component instantiation
+    cu : control_unit
+        port map (
+            clk => clk,
+            rst => rst,
+            state => cu_state,
+            pc_wr => pc_wr_s
+        );
     pc : program_counter
         port map (
             clk => clk,
-            wr_en => wr_en,
+            wr_en => pc_wr_s,
             rst => rst,
             data_out => data_out_s
         );
@@ -46,4 +66,5 @@ begin
             address => data_out_s,
             data => test_data_rom_out
         );
+    
 end architecture test_a;
