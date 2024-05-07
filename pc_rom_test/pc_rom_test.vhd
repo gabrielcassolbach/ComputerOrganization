84853b4e05afdbec56bec12395e16f2b99c007a5
Adame <gusadomingues@gmail.com>
2024-05-07 18:44:35 -0300

    feat - top lvl vhdl testing jumps and nop

--- pc_rom_test/pc_rom_test.vhd
+++ pc_rom_test/pc_rom_test.vhd
@@ -7,7 +7,7 @@ entity test is
         clk : in std_logic;
         rst : in std_logic;
         cu_state : out std_logic;    -- for debugging
-        test_data_rom_out : out unsigned(15 downto 0)
+        rom_data_out : out unsigned(15 downto 0)
     );
 end entity test;
 
@@ -18,6 +18,7 @@ architecture test_a of test is
             clk : in std_logic;
             wr_en : in std_logic;
             rst : in std_logic;
+            increment : in unsigned(6 downto 0);
             data_out : out unsigned(6 downto 0)
         );
     end component program_counter;
@@ -34,36 +35,64 @@ architecture test_a of test is
         port(
             clk     : in std_logic;
             rst     : in std_logic;
+            opcode  : in unsigned(3 downto 0);
             state   : out std_logic;    -- for debugging
-            pc_wr   : out std_logic
+            pc_wr   : out std_logic;
+            jump_sel: out std_logic;
+            nop_sel : out std_logic
         );
     end component control_unit;
     
     -- signal declarations
-    signal data_out_s: unsigned(6 downto 0);
+    
+    -- program counter signals
+    signal pc_adress_out_s: unsigned(6 downto 0);
+    signal pc_increment_s: unsigned(6 downto 0);
+    -- control unit signals
     signal pc_wr_s: std_logic;
-
+    signal jump_sel_s: std_logic;
+    signal nop_sel_s: std_logic;
+    -- instruction partition signals (missing a lot of signals here)
+    signal rom_data_out_s: unsigned(15 downto 0);
+    signal opcode_s: unsigned(3 downto 0);
+    signal instruction_address_s: unsigned(6 downto 0);
+    
 begin
     -- component instantiation
     cu : control_unit
         port map (
             clk => clk,
             rst => rst,
+            opcode => opcode_s,
             state => cu_state,
-            pc_wr => pc_wr_s
+            pc_wr => pc_wr_s,
+            jump_sel => jump_sel_s,                
+            nop_sel => nop_sel_s
         );
     pc : program_counter
         port map (
             clk => clk,
             wr_en => pc_wr_s,
             rst => rst,
-            data_out => data_out_s
+            increment => pc_increment_s,
+            data_out => pc_adress_out_s
         );
     mem_rom : rom
         port map (
             clk => clk,
-            address => data_out_s,
-            data => test_data_rom_out
+            address => pc_adress_out_s,
+            data => rom_data_out_s
         );
+
+    -- rom instrucion partition
+    opcode_s <= rom_data_out_s(15 downto 12);
+    instruction_address_s <= rom_data_out_s(6 downto 0);
+
+    -- pc increment calculation
+    pc_increment_s <=   "0000001" when (jump_sel_s = '0' or nop_sel_s = '1') else               --logic on this line will change when more instructions are added
+                        (instruction_address_s - pc_adress_out_s) when jump_sel_s = '1' else
+                        "0000001";                                                              --default increment (may change later on)
+
+    rom_data_out <= rom_data_out_s;
     
 end architecture test_a;
--- pc_rom_test/pc_rom_test_tb.vhd
+++ pc_rom_test/pc_rom_test_tb.vhd
@@ -9,53 +9,33 @@ architecture testbench of pc_rom_test_tb is
     component test is
         port (
             clk : in std_logic;
-            -- pc_wr : in std_logic;
             rst : in std_logic;
             cu_state : out std_logic;    -- for debugging
-            test_data_rom_out : out unsigned(15 downto 0)
+            rom_data_out : out unsigned(15 downto 0)
         );
     end component test;
     
     signal clk : std_logic;
-    -- signal wr_en : std_logic;
     signal rst : std_logic;
-    -- signal test_data_pc_in : unsigned(6 downto 0);
     signal cu_state : std_logic;
-    signal test_data_rom_out : unsigned(15 downto 0);
+    signal rom_data_out : unsigned(15 downto 0);
 
     begin
         uut : test port map (
             clk => clk,
-            -- wr_en => wr_en,
-            cu_state => cu_state,
             rst => rst,
-            test_data_rom_out => test_data_rom_out
+            cu_state => cu_state,
+            rom_data_out => rom_data_out
         );
         process
         begin
-            clk <= '0';
-            -- wr_en <= '1';
-            wait for 5 ns;
-            clk <= '1';
-            wait for 5 ns;
+            -- Loop through address o and 5
             clk <= '0';
             wait for 5 ns;
             clk <= '1';
             wait for 5 ns;
             clk <= '0';
             wait for 5 ns;
-            clk <= '1';
-            wait for 5 ns;
-            clk <= '0';
-            wait for 5 ns;
-            clk <= '1';
-            wait for 5 ns;
-            clk <= '0';
-            wait for 5 ns;
-            clk <= '1';
-            wait for 5 ns;
-            clk <= '0';
-            wait;
         end process;
 
 end architecture testbench;
