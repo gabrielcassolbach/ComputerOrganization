7b78f26f304a8d126fa786ca1449e49d6a70d168
Adame <gusadomingues@gmail.com>
2024-05-07 18:38:08 -0300

    feat - control unit detecting jump & nop

--- control_unit/control_unit.vhd
+++ control_unit/control_unit.vhd
@@ -5,10 +5,13 @@ use ieee.numeric_std.all;
 
 entity control_unit is 
     port(
-        clk     : in std_logic;
-        rst     : in std_logic;
-        state   : out std_logic;    -- for debugging
-        pc_wr   : out std_logic
+        clk         : in std_logic;
+        rst         : in std_logic;
+        opcode      : in unsigned(3 downto 0);      -- opcode from instruction memory (ROM)
+        state       : out std_logic;                -- for debugging
+        pc_wr       : out std_logic;
+        jump_sel    : out std_logic;                -- jump signal
+        nop_sel     : out std_logic                 -- no operation signal
     );
 end control_unit;
 
@@ -33,8 +36,17 @@ architecture control_unit_a of control_unit is
             rst    => rst,
             state  => state_s
         );
+        
+        -- instruction format: 16 bits
+        -- 15-12: opcode
+        -- 11-0: to be defined ...
 
         -- Output signals
-        state <= state_s;
         pc_wr <= '1' when state_s = '1' else '0';
+
+        jump_sel <= '1' when opcode = "1111" else '0'; -- inconditional jump (absolute)
+        nop_sel  <= '1' when opcode = "0000" else '0'; -- no operation
+        
+        state <= state_s;
+
 end control_unit_a;
