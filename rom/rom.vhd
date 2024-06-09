c64824975831f6e3dafc542a1c7386ddd11bd3bb
gabrielbach <gabrielcassolbach@gmail.com>
2024-06-09 17:01:06 -0300

    setting all values in rom in the inteval: (1 -> 32)

--- rom/rom.vhd
+++ rom/rom.vhd
@@ -13,13 +13,24 @@ end entity rom;
 architecture a_rom of rom is
     type memory is array (0 to 127) of unsigned(15 downto 0);
     constant rom_data : memory := (
-        0  => B"0011_0011_00011110",   -- LI R3, 30 -- Endereço inicial. -- ok
-        1  => B"0011_0010_00001010",   -- LI R2, 10 -- Valor que será colocado na RAM. -- ok
-        2  => B"1100_1000_0010_0000",  -- MOV A, R2 -- Valor  -- ok.
-        3  => B"0010_0011_00000000",   -- SW ($R3) -> (Store.) Carrega o valor do acumulador na RAM no endereço apontado por R3. 
-        4  => B"1100_1000_0001_0000",   -- MOV A, R1 -- Zera o Acumulador.
-        5  => B"0110_0011_00000000",   -- LW ($R3) -> 
-        6  => B"1100_0101_1000_0000",  -- MOV R5, A -- deve aparecer 10.
+        0  => B"0011_0001_00000001", -- LI R1, 1 -- Valor inicial. (address and value)
+        1  => B"0011_0010_00000001", -- LI R2, 1 -- Incremento.
+        2  => B"0011_0111_00100001", -- LI R7, 33 -- Final do loop 
+        3  => B"1100_1000_0001_0000", -- MOV A, R1
+        4  => B"0010_0001_00000000",  -- SW ($R1)
+        5  => B"0100_0000_0010_0000", -- ADD R2 -- incrementa acumulador.
+        6  => B"1100_0001_1000_0000", -- MOV R1, A.
+        7  => B"0001_0000_0111_0000", -- CMP R7
+        8  => B"0111_0000_11111010", -- JL -6
+        9  => B"0011_0110_00000001", -- LI R6, 1
+        --10  => B"",
+        --11  => B"",
+        --12  => B"",
+        --13  => B"",
+        --14  => B"",
+        --15  => B"",
+        --16  => B"",
+        --17  => B"",   
         others => (others=>'0')
     );  
 begin
